29ef446f6fbac7d727f2bb28367cc5426e41b3e9
LanKuDot <lankudot@github.com>
2015-10-01 15:16:14 +0800

    Generate data for time plot

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#ifdef TIME_PLOT
+  #include <time.h>
+#endif
 
 extern int fibonacci(int x);
 
@@ -7,8 +10,23 @@ int main(int argc, char **argv)
   int number=0;
   int result=0;
 
+#ifdef TIME_PLOT
+  FILE *fp = fopen( "recursive.txt", "w" );
+  clock_t cpuTick = 0;
+  for ( number = 1; number < 31; ++number )
+  {
+	cpuTick = clock();
+	fibonacci(number);
+	cpuTick = clock() - cpuTick;
+	fprintf( fp, "%d\t%ld\n", number, (long)cpuTick );
+  }
+  fclose( fp );
+#else
   scanf("%d",&number);
   result = fibonacci(number);   
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+#endif
+
+  return 0;
 }
 
